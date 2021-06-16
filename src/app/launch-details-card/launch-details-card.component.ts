c63093e365ad16887f0abfedc3c51f6566701ee1
karlivanr <kremonde@gmail.com>
2021-06-16 14:37:59 +0800

    Progress Update
    -tasks 1 and 2 done
    -tasks 7 partially done

--- src/app/launch-details-card/launch-details-card.component.ts
+++ src/app/launch-details-card/launch-details-card.component.ts
@@ -1,3 +1,4 @@
+import { Input, OnChanges } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './launch-details-card.component.html',
   styleUrls: ['./launch-details-card.component.scss']
 })
-export class LaunchDetailsCardComponent implements OnInit {
+export class LaunchDetailsCardComponent implements OnInit, OnChanges {
+  @Input() data: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges() {
+    console.log(this.data);
+  }
 }
