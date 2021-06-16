ed3226cdda9888e65d0f925175423ab119855030
karlivanr <kremonde@gmail.com>
2021-06-16 13:02:17 +0800

    Title Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchProgramsComponent } from './launch-programs/launch-programs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchProgramsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
