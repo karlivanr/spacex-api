fe03e83393ae96407b885cd930c0944a914b5d28
karlivanr <kremonde@gmail.com>
2021-06-16 18:24:17 +0800

    Latest Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
